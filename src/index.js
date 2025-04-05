59579229e669a3f99462b8358b23e2e429e4a87a
keyurOd <Keyurod20@gmail.com>
2025-04-05 19:58:43 -0400

    profile picture update

--- src/index.js
+++ src/index.js
@@ -12,12 +12,10 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://one01413667-comp3133-assignment2-frontend.onrender.com',
-    credentials: true
-  }));
-
+  origin: 'https://one01413667-comp3133-assignment2-frontend.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
-app.use(verifyToken);
 
 
 const storage = multer.diskStorage({
@@ -28,7 +26,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10000000 } 
+  limits: { fileSize: 10000000 }
 }).single('profilePicture');
 
 
@@ -38,13 +36,16 @@ app.post('/upload', (req, res) => {
       return res.status(400).json({ message: err.message });
     }
 
-    res.json({ filePath: `/uploads/${req.file.filename}` });
+    const baseUrl = process.env.NODE_ENV === 'production'
+      ? 'https://one01413667-comp3133-assignment2-backend.onrender.com'
+      : `http://localhost:${config.PORT}`;
+    const filePath = `${baseUrl}/uploads/${req.file.filename}`;
+    res.json({ filePath });
   });
 });
 
+
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
-
 
 async function startServer() {
   const server = new ApolloServer({
@@ -52,12 +53,14 @@ async function startServer() {
     resolvers,
     cache: "bounded",
     context: ({ req }) => ({ user: req.user }),
-    introspection: true, 
-    playground: true
+    introspection: true,
+    playground: true,
   });
   await server.start();
   server.applyMiddleware({ app, path: '/graphql' });
 
+
+  app.use(verifyToken);
 
   mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
